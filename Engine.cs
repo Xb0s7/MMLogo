a39f604fc5611f7fbf36ecb2a93d0760bb0459f4
Xb0s7 <58825475+Xb0s7@users.noreply.github.com>
2020-02-13 00:58:06 +0200

    Add files via upload

---
+++ Engine.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text;
+using mmLogo.Contracts;
+using mmLogo.Models;
+
+namespace mmLogo
+{
+    public class Engine
+    {
+        private IDrawable drawer;
+        public Engine()
+        {
+            drawer = new LogoDraw();
+        }
+        public void Run()
+        {
+            int size = int.Parse(Console.ReadLine());
+            Console.WriteLine(drawer.Draw(size));
+        }
+    }
+}
---
+++ StartUp.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using mmLogo;
+namespace MMLogo
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Engine eng = new Engine();
+            eng.Run();
+        }
+    }
+}
